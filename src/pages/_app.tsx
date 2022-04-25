7cfa1f5c859dbb25b3d1f28791264a6826bc69e2
Rohidul <rohidul209@gmail.com>
2022-04-25 12:14:41 +0600

    Head title added "Portfolio"

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,20 @@
 import "src/styles/globals.css";
 import ColorSchemeProvider from "src/contexts/color-scheme";
 import { CustomAppProps } from "src/types";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: CustomAppProps) {
   const getLayout = Component.getLayout ?? ((page) => page);
 
   return (
-    <ColorSchemeProvider>
-      {getLayout(<Component {...pageProps} />)}
-    </ColorSchemeProvider>
+    <>
+      <Head>
+        <title>Portfolio</title>
+      </Head>
+      <ColorSchemeProvider>
+        {getLayout(<Component {...pageProps} />)}
+      </ColorSchemeProvider>
+    </>
   );
 }
 
