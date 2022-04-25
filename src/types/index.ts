59fa57d925c1221760f1a7b6e00bff12d4b8bb2e
Rohidul <rohidul209@gmail.com>
2022-04-25 20:22:00 +0600

    types moved to next.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,11 +1,1 @@
-import { NextPage } from "next";
-import { AppProps } from "next/app";
-import { ReactElement, ReactNode } from "react";
-
-export type CustomNextPage = NextPage & {
-  getLayout?: (page: ReactElement) => ReactNode;
-};
-
-export type CustomAppProps = AppProps & {
-  Component: CustomNextPage;
-};
+export * from "./next";
